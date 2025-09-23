cb3904d6fc926516f472f4715beec9d844d41a0f
Andrew Johnston <asjohnston@alaska.edu>
2025-09-23 09:10:37 -0800

    submit jobs in parallel

--- submit_jobs.py
+++ submit_jobs.py
@@ -1,17 +1,24 @@
 import os
 from datetime import datetime, timedelta
 from uuid import uuid4
+from concurrent.futures import ThreadPoolExecutor
 
 import boto3
 
 os.environ['AWS_PROFILE'] = 'grfn'
-sqs = boto3.client('sqs')
 queue_url = 'https://sqs.us-east-1.amazonaws.com/406893895021/asj-logs-dev-Queue-oAM46NwYO4CV'
 
 
 def chunks(lst, n=10):
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
+
+
+def submit_batch(batch: list[str]) -> None:
+    sqs = boto3.client('sqs')
+    for chunk in chunks(batch, 10):
+        messages = [{'Id': str(uuid4()), 'MessageBody': prefix} for prefix in chunk]
+        sqs.send_message_batch(QueueUrl=queue_url, Entries=messages)
 
 
 interval = timedelta(minutes=1)
@@ -23,9 +30,5 @@ while current < end:
     prefixes.append(f's3-access/grfn-content-prod/{current.strftime("%Y-%m-%d-%H-%M")}')
     current += interval
 
-ii = 0
-for chunk in chunks(prefixes):
-    messages = [{'Id': str(uuid4()), 'MessageBody': prefix} for prefix in chunk]
-    sqs.send_message_batch(QueueUrl=queue_url, Entries=messages)
-    ii += len(messages)
-    print(f'{ii}/{len(prefixes)}')
+with ThreadPoolExecutor(max_workers=5) as executor:
+    executor.map(submit_batch, chunks(prefixes, 1000))
