d7a8c73ee49a5d99bab9a900bab0cbfbb7a70d92
Andrew Johnston <asjohnston@alaska.edu>
2025-09-26 14:15:51 -0800

    revise dates and tiers

--- computer.py
+++ computer.py
@@ -3,14 +3,13 @@ from concurrent.futures import ProcessPoolExecutor
 from datetime import datetime, timedelta
 import json
 
-start = datetime(2025, 9, 20)
-end = datetime(2025, 9, 23)
+start = datetime(2024, 10, 1)
+end = datetime(2025, 9, 30)
 num_days = (end - start).days
 dates = [(start + timedelta(days=x)).strftime('%Y-%m-%d') for x in range(num_days)]
 
 with open('aggregated.json') as f:
     granules = json.load(f)
-
 
 def chunks(lst, n=10):
     for i in range(0, len(lst), n):
@@ -36,34 +35,35 @@ def process_granules(granules: list[dict]) -> defaultdict:
                 delta = 0
 
             if delta < 30:
-                tier = '0 - frequent'
+                tier = '0-30 days'
             elif delta < 90:
-                tier = '1 - infrequent'
+                tier = '30-90 days'
             elif delta < 180:
-                tier = '2 - archive instant'
+                tier = '90-180 days'
             else:
-                tier = '3 - deep archive'
+                tier = '>180 days'
 
-            platform = granule['n'][:3]
-            beam_mode = granule['n'][4:6]
+            month = dt[:7]
+            platform = granule['n'][2]
+            beam_mode = granule['n'][4]
             product_type = granule['n'][7:10]
             dual_single = granule['n'][14]
             polarization = granule['n'][15]
-            acquisition_month = granule['n'][17:23]
-            output[f'{dt},{tier},{platform},{beam_mode},{product_type},{dual_single},{polarization},{acquisition_month}'] += granule['s']
+            acquisition_year = granule['n'][17:21]
+            output[f'{month},{tier},{platform},{beam_mode},{product_type},{dual_single},{polarization},{acquisition_year}'] += granule['s']
 
             delta += 1
     return output
 
 
 output = defaultdict(int)
-with ProcessPoolExecutor() as executor:
+with ProcessPoolExecutor(max_workers=10) as executor:
     for result in executor.map(process_granules, chunks(granules, 10000)):
         for label, volume in result.items():
             output[label] += volume
 
 
 with open('tiers.csv', 'w') as f:
-    f.write('date,tier,platform,beam mode,product type,dual/single,polarization,acquisition month,volume\n')
+    f.write('month,tier,platform,beam mode,product type,dual/single,polarization,acquisition year,volume\n')
     for label, volume in output.items():
         f.write(f'{label},{volume}\n')
