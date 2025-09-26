5d573aa6338b479e020e780d1a9069225f48db1b
Andrew Johnston <asjohnston@alaska.edu>
2025-09-26 12:46:11 -0800

    parallelize aggregate sort

--- aggregate.sh
+++ aggregate.sh
@@ -1,6 +1,6 @@
 aws --profile edc-sandbox s3 sync s3://asj-s1-logs-bucket-udg18agd3uci/ .
 
-find . | xargs cat | sort | uniq > ../access_dates.csv
+find . | xargs cat | sort --parallel=20 --buffer-size=50G | uniq > ../access_dates.csv
 
 aws s3 ls s3://asf-ngap2w-p-s1-raw-98779950/ | cut -c1-10,20-999 | sed 's/ \+/,/g' > inventory.csv
 aws s3 ls s3://asf-ngap2w-p-s1-grd-7d1b4348/ | cut -c1-10,20-999 | sed 's/ \+/,/g' >> inventory.csv
