ea957148b9782959b3079f9fa34d46ffc6326414
Andrew Johnston <asjohnston@alaska.edu>
2025-10-03 10:05:49 -0800

    add bytes and user id to lambda app output

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import csv
 import json
 import os
 import traceback
+from collections import defaultdict
 from datetime import datetime
 
 import boto3
@@ -23,6 +24,17 @@ def get_keys(bucket: str, prefix: str) -> list[str]:
     return [item['Key'] for page in page_iterator for item in page.get('Contents', [])]
 
 
+def get_user(line: list[str]) -> str:
+    if 'DownloadRole' in line[5]:
+        user_id = line[5].split('/')[-1]
+        if '@' in user_id and user_id.split('@')[-1].isdigit():
+            user_id = user_id.split('@')[0]
+        return user_id
+    if '&A-userid=' in line[9]:
+        return line[9].split(' ')[1].split('=')[-1]
+    return line[5].split(':')[-1]
+
+
 def get_log_events(bucket: str, key: str) -> list[dict]:
     response = INPUT_S3_CLIENT.get_object(Bucket=bucket, Key=key)
     log_content = response['Body'].read().decode('utf-8')
@@ -33,19 +45,22 @@ def get_log_events(bucket: str, key: str) -> list[dict]:
             'status_code': line[10],
             'key': line[8],
             'date': datetime.strptime(line[2][1:12], '%d/%b/%Y'),
+            'bytes': 0 if line[12] == '-' else int(line[12]),
+            'user': get_user(line),
         } for line in reader if line
     ]
 
 
 def process_product(log_bucket_name: str, prefix: str, output_bucket_name: str) -> None:
     keys = get_keys(log_bucket_name, prefix)
-    output = set()
+    output = defaultdict(int)
     for key in keys:
         for log_event in get_log_events(log_bucket_name, key):
             if log_event['method'] in ('REST.GET.OBJECT', 'REST.COPY.PART_GET', 'REST.COPY.OBJECT_GET') and log_event['status_code'] in ('200', '206'):
-                output.add(f'{log_event["key"]},{log_event["date"].strftime("%Y-%m-%d")}\n')
+                output[f'{log_event["key"]},{log_event["date"].strftime("%Y-%m-%d")},{log_event["user"]}'] += log_event['bytes']
     if output:
-        OUTPUT_S3_CLIENT.put_object(Bucket=output_bucket_name, Key=prefix, Body=''.join(output))
+        body = '\n'.join(f'{key},{value}' for key, value in output.items())
+        OUTPUT_S3_CLIENT.put_object(Bucket=output_bucket_name, Key=prefix, Body=body)
 
 
 def lambda_handler(event: dict, _) -> dict:
